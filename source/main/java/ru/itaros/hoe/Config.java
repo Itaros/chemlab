6bcf1587a4775b3a0f63f46d2053b9d4f13529f6
Semion Nadezhdin <me@sg-studio.ru>
2015-04-08 20:49:06 +0300

    Added: HOEKA warning to cfg comments

--- source/main/java/ru/itaros/hoe/Config.java
+++ source/main/java/ru/itaros/hoe/Config.java
@@ -9,7 +9,8 @@ public class Config {
 	private static final String THREADING_KEEPALIVE_COMMENTARY = "This option allows you to engage HOE MTA Monitor. "
 			+ "\nIt will keep all machines working even after chunk being unloaded,"
 			+ "\n but it still needs to load that chunk at least once to catchup(for now, ^___^)!"
-			+ "\nWARNING: HOE KeepAlive may break worlds in SSP! Use only with dedicated server!";
+			+ "\nWARNING: HOE KeepAlive may break worlds in SSP! Use only with dedicated server!"
+			+ " Actually, DO NOT ENABLE IT AT ALL!";
 	private static final String THREADING_RETENTION_COMMENTARY = ""
 			+ "This option defines retention policy for HOE Threads."
 			+ "\nNONE - no retention. Useful to keep performance on NUMA architectures(was unconfigurable default from initial release to PR-2.03i2)"
